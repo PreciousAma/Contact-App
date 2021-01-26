342ba6e0b9371b9fa8b6ece917b351ef966291b9
AmaraCP <preshchilaka06@gmail.com>
2021-01-26 15:36:07 +0100

    counter-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const Title = () => {
+  return (
+    <h1>Contact Form</h1>
+  );
+}
+
+const Button = () => {
+  return (
+    <button>submit</button>
+  );
+}
+
+const App = () => {
+  const [title, setTitle] = useState('Contact app');
+
+  const handleChangeTitle = () => {
+    setTitle('Contact Form');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      React Functional Component
+
+      <h2>{title}</h2>
+      <button onClick={handleChangeTitle}>Change title</button>
     </div>
   );
 }
